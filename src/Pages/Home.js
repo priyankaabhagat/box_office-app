d8e483e882ae38af77ee15d98cb4d5547641a4b0
Priyanka bhagat <priyankabhagat2721@gmail.com>
2022-07-12 18:27:50 +0530

    displayed show results and fetch refractor

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 // eslint-disable-next-line import/no-cycle
 import MainPageLayout from '../components/MainPageLayout';
+// eslint-disable-next-line import/no-unresolved
+import { apiGet } from '../misc/Config';
 
 function Home() {
   const [input, setInput] = useState('');
   // console.log(input);
+  const [results, setResult] = useState(null);
 
   function onInputChange(eventObj) {
     // console.log(event.target.value);
@@ -12,11 +15,10 @@ function Home() {
   }
 
   function onSearch() {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(response => response.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResult(result);
+      console.log(result);
+    });
   }
 
   function onKeyDown(event) {
@@ -24,6 +26,29 @@ function Home() {
       // console.log(event.keyCode);
       onSearch();
     }
+  }
+
+  function renderResults() {
+    if (results && results.length === 0) {
+      // this is when u type,unusual and api doesn't return anything.
+      return <div>No Results Found.</div>;
+    }
+
+    if (results && results.length > 0) {
+      // when something is returned.
+
+      return (
+        <div>
+          {' '}
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    // default case when above two cases doesn't occur
+    return null;
   }
 
   // return <div>GO TO HOME</div>;
@@ -38,6 +63,7 @@ function Home() {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 }
