2ebe67c8d0e0d905abc1e7c27e1a17e2f912ff86
Priyanka bhagat <priyankabhagat2721@gmail.com>
2022-07-14 13:11:36 +0530

    replaced useState with useReducer on the show page

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,29 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router';
 import { apiGet } from '../misc/Config';
 
 function Show() {
-  const { id } = useParams();
-  console.log(id);
+  // eslint-disable-next-line consistent-return
+  function reducer(prevState, action) {
+    switch (action.type) {
+      case 'FETCH_SUCCESS': {
+        return { isLoading: false, error: null, show: action.show };
+      }
+      case 'FETCH_FAILED': {
+        return { ...prevState, isLoading: false, error: action.error };
+      }
+      // eslint-disable-next-line no-unused-expressions
+      default:
+        return prevState;
+    }
+  }
 
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { id } = useParams();
+
+  const initialState = {
+    show: null,
+    isLoading: true,
+    error: null,
+  };
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast `)
       .then(results => {
-        // eslint-disable-next-line no-unused-expressions
-
+        // if fetch is successful
         if (isMounted) {
-          setShow(results);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
       })
+      // if fetch unsuccessful
       .catch(err => {
-        setError(err.message);
-        setIsLoading(false);
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
       });
 
     return () => {
