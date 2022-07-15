295b6247019346414e11ba34abd35ecb5a93d2ca
Priyanka bhagat <priyankabhagat2721@gmail.com>
2022-07-15 20:03:38 +0530

    finished starred page

--- src/Pages/Starred.js
+++ src/Pages/Starred.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { useShows } from '../misc/custom-hooks';
+import { apiGet } from '../misc/Config';
+import ShowGrid from '../components/shows/ShowGrid';
 
 function Starred() {
-  // return <div>THIS IS STARRED</div>;
-  return <MainPageLayout>THIS IS STARRED</MainPageLayout>;
+  // we will destructure the starred shows from useShows() hooks
+  const [starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}?`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {/* these below lines are basically the conditons are defined. */}
+      {isLoading && <div>Shows are still loading...</div>}{' '}
+      {error && <div>Error Occurred : {error}</div>}{' '}
+      {!isLoading && !shows && <div>No Shows were Added.</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 }
 
 export default Starred;
